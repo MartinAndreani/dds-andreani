04edac2303d14489680831ab26ad161a1f908eeb
MartinAndreani <138808842+MartinAndreani@users.noreply.github.com>
2023-11-15 01:25:44 -0300

    Add files via upload

---
+++ Simulacro3K2-base/api-backend-pap/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// leer archivo de configuracion
+require('dotenv').config();
+
+// para poder leer json en el body
+app.use(express.json()); 
+
+// Configuración de CORS
+app.use(cors());
+
+// Inicio del servidor
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Servidor iniciado en el puerto ${port}`);
+});
+
+module.exports = app;
+
